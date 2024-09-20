1c6cc8e7f746cab83dc3d36440fa851c100fd3c2
sanyogsr <sanyogsingrajput@gmail.com>
2024-09-20 14:08:00 +0530

    check

--- lib/auth.ts
+++ lib/auth.ts
@@ -5,6 +5,7 @@ import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import prisma from "./db";
 
 export const authOptions: NextAuthOptions = {
+  secret: process.env.NEXTAUTH_SECRET,
   adapter: PrismaAdapter(prisma),
   providers: [
     EmailProvider({
